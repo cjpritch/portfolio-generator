ba10fa8bac068c24992a22189aef29a18877d81a
Carly Pritchard <cpritch616@gmail.com>
2022-06-27 19:47:54 -0500

    generate html w node

--- app.js
+++ app.js
@@ -1,15 +1,60 @@
-const profileDataArgs = process.argv.slice(2);
+// Required statement to use file system module 
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+// Holds user command line arguments
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-  console.log('================');
+// Store arguments in variables 
+const [name, github] = profileDataArgs;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// Create html 
+const generatePage = (name, github) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
 
-printProfileData(profileDataArgs);
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
+}; 
+
+// Generate html file 
+fs.writeFile("index.html", generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log("Portfolio complete!");
+});
+
+
+
+
+
+
+
+
+
+
+// const profileDataArgs = process.argv.slice(2);
+
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
