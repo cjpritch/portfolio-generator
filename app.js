b68088bb2ce217012b8eb9105ffc3f2b23f46e8d
Carly Pritchard <cpritch616@gmail.com>
2022-07-09 16:14:17 -0500

    refactor code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template.js')
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -64,7 +64,7 @@ if (!portfolioData.projects) {
   return inquirer.prompt([
     {
       type: 'input',
-      name: 'projects',
+      name: 'name',
       message: 'What is the name of your project? (Required)',
       validate: nameInput => {
         if (nameInput) {
@@ -133,13 +133,20 @@ if (!portfolioData.projects) {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-     fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-       console.log('Page created! Check out index.html in this directory to see it!');
-     });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
 
 
